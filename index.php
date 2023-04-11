7f227849a089d8aa3b4eb545687809b91b7fafef
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-04-11 15:54:00 +0200

    generate random password with letters, symbols and numbers

--- index.php
+++ index.php
@@ -13,6 +13,35 @@
             return "La tua password è lunga $password_length caratteri.";
         }
     }
+
+    // Generate a random password
+    function generatePassword($length) {
+        $random_password = "";
+
+        while (strlen($random_password) < $length) {
+            $category_choice = rand(1, 4);
+
+            if ($category_choice == 1) {
+                // Lowercase letters
+                $letter = chr(rand(97, 122));
+                $random_password .= $letter;
+            } elseif ($category_choice == 2) {
+                // Uppercase letters
+                $letter_upper = chr(rand(65, 90));
+                $random_password .= $letter_upper;
+            } elseif ($category_choice == 3) {
+                // Numbers
+                $number = chr(rand(48, 57));
+                $random_password .= $number;
+            } else {
+                // Symbols
+                $symbol = chr(rand(40, 47));
+                $random_password .= $symbol;
+            }
+        }
+
+        return $random_password;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -37,6 +66,8 @@
     <!-- Password generator -->
     <div class="password">
         <?php echo checkPassword($_GET["password-length"]) ?>
+        <br>
+        <?php echo generatePassword($_GET["password-length"]) ?>
     </div>
     <!-- // Password generator -->
 </body>
